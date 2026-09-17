7885af009256e05161161018f6a828dec049a627
gearandgo <gearandgo@email.cz>
2026-09-17 12:25:18 +0200

    Delete dcc-watch/dcc-watch directory

--- dcc-watch/dcc-watch/docs/sw.js
+++
@@ -1,5 +0,0 @@
-// Minimální service worker: appku jde nainstalovat na plochu, data se berou vždy čerstvá ze sítě.
-self.addEventListener("install", () => self.skipWaiting());
-self.addEventListener("fetch", e => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
-});
